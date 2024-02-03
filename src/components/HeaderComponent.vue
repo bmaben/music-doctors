504cf717955d69a052b4bd0ed67077666184cdd6
bmaben <baileymaben1@gmail.com>
2024-02-03 00:51:45 -0500

    scaffolding for project

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page-title">
+    <h1>Music Doctors</h1>
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.page-title  {
+ height: 100%;
+ width: 100%;
+}
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <HeaderView />
+
+</template>
+
+<script>
+import HeaderView from '../views/HeaderView.vue'
+export default {
+  components: { HeaderView }
+    
+}
+</script>
+
+<style>
+
+</style>
